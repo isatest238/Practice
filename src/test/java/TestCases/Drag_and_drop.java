1e0ac0450da03c56ecb42674dd0205427973b43b
isatest238 <isatest238@gmail.com>
2024-12-30 14:30:57 +0200

    drag and drop

---
+++ src/test/java/TestCases/Drag_and_drop.java
@@ -0,0 +1,37 @@
+package TestCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class Drag_and_drop {
+    public WebDriver driver;
+
+    @Test
+    public void dragAndDropMethod(){
+        //deschidem un Chrome browser
+        driver = new ChromeDriver();
+
+        //accesam o pagina web
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+
+        // facem browserul in modul maximized
+        driver.manage().window().maximize();
+
+        // Click on 'Drag and drop' menu option
+        WebElement dragAndDropElement = driver.findElement(By.xpath("//a[text()='Drag and drop']"));
+        dragAndDropElement.click();
+
+        WebElement draggableElement = driver.findElement(By.id("draggable"));
+        WebElement dropElement = driver.findElement(By.id("target"));
+
+        // Create an Actions object
+        Actions actions = new Actions(driver);
+
+        // Perform drag-and-drop using clickAndHold and moveToElement
+        actions.clickAndHold(draggableElement).moveToElement(dropElement).release().perform();
+    }
+}
