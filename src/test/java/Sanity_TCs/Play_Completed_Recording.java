2250704af05876ca0a7255af0a1d3e5e80028764
isatest238 <isatest238@gmail.com>
2025-04-06 21:51:37 +0300

    Play Live TV test case

--- src/test/java/Sanity_TCs/Play_Completed_Recording.java
+++ src/test/java/Sanity_TCs/Play_Completed_Recording.java
@@ -12,13 +12,10 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-import static org.testng.Assert.assertTrue;
-
-public class Play_LiveTV_from_EPG {
+public class Play_Completed_Recording {
     public WebDriver driver;
-
     @Test
-    public void Play_LiveTV_from_EPGMethod() throws InterruptedException {
+    public void recordingMethod (){
         //deschidem un Chrome browser
         driver = new ChromeDriver();
 
@@ -81,70 +78,9 @@ public class Play_LiveTV_from_EPG {
         // Send ESC key globally
         actions.sendKeys(Keys.ESCAPE).perform();
 
-        System.out.println("User successfully on the MagentaTV Start Page");
-
-        // Identify EPG icon locators and click on it
-        WebElement epgIconElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@id='MENU-EPG']")));
-        epgIconElement.click();
+        System.out.println("User successfully logged in");
 
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
-        //close first overlay
-        WebElement firstOverlayElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//BUTTON[@ID='OVERLAY-CLOSE' and contains(@class, 'oqaYO st41f jTZzO')]")));
-        firstOverlayElement.click();
-        // close second overlay
-        WebElement secondOverlayElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//BUTTON[@ID='OVERLAY-CLOSE' and contains(@class, 'oqaYO st41f jTZzO')]")));
-        secondOverlayElement.click();
 
-        // Identify program logo and click on it (KIKA e.g)
-        WebElement kikaElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@id='epg_ch_262282280172']")));
-        kikaElement.click();
-
-        Thread.sleep(8000);
-
-        // hover pe coordonate (x=200, y=300)
-        actions.moveByOffset(200, 300).perform();
-
-        //take the time into a variable right after starting the play
-        WebElement durationPlayerElement = driver.findElement(By.xpath("//div[@class='MRdfJ']"));
-        String startDuration = durationPlayerElement.getText();
-        System.out.println("The time when the video starts to play is " + startDuration);
-
-
-        Thread.sleep(10000);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5080));
-
-
-        // hover pe coordonate (x=200, y=300)
-        actions.moveByOffset(200, 300).perform();
-
-        //take the time into a variable after playing 5 seconds y
-        WebElement durationPalyerActualElement = driver.findElement(By.xpath("//div[@class='MRdfJ']"));
-        String startDurationAfterPlay = durationPalyerActualElement.getText();
-        System.out.println("The time after the video played for a while is " + startDurationAfterPlay);
-
-
-        // convert into seconds the values from startDuration and startDurationAfterPlay
-        int totalSecondsinitial = extractSeconds(startDuration);
-        System.out.println(totalSecondsinitial);
-        int totalSecondAfterPlay = extractSeconds(startDurationAfterPlay);
-        System.out.println(totalSecondAfterPlay);
-
-
-        assertTrue(totalSecondsinitial > totalSecondAfterPlay, startDurationAfterPlay + "should be after" + startDuration);
-
-        //Identify play button and click on it to pause playback of the stream
-        WebElement pauseButtonElement = driver.findElement(By.xpath("//button[@id='PLAYER-PLAY']"));
-        pauseButtonElement.click();
-    }
-
-    public static int extractSeconds(String input) {
-        // Extract just the time part using regex
-        String cleaned = input.replaceAll("[^0-9:]", "");  // removes 'Noch', spaces etc.
-        String[] parts = cleaned.split(":");
-        int minutes = Integer.parseInt(parts[0]);
-        int seconds = Integer.parseInt(parts[1]);
-        return minutes * 60 + seconds;
     }
 }
-
